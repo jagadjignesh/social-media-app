db892089fa8352bcc9c7fe33b74aeab0f092bb3a
jagadjignesh <jagadjignesh27@gmail.com>
2025-08-01 19:37:14 +0530

    Add live backend url

--- frontend/src/contexts/AppContext.jsx
+++ frontend/src/contexts/AppContext.jsx
@@ -5,7 +5,7 @@ export const AppContext = createContext();
 export const AuthProvider = (props) => {
     
     const [isLoggedIn , setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn") === "true");
-    const [siteurl , setsiteurl] = useState("http://localhost:4001");
+    const [siteurl , setsiteurl] = useState("https://social-media-app-backend-yk5m.onrender.com");
 
     return (
         <AppContext.Provider value={{isLoggedIn, setIsLoggedIn, siteurl}}>
