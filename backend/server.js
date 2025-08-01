8f538c252960458ca9b270a73908e8437cf43f83
jagadjignesh <jagadjignesh27@gmail.com>
2025-08-01 19:58:13 +0530

    change live url on backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://social-media-app-frontend-7qbh.onrender.com',
   credentials: true,
 }));
 app.use(cookieParser());
